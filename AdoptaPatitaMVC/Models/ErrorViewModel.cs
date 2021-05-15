f2050c73c1e1d6e7770eea56ca79d2bc81571388
magno309 <48113838+magno309@users.noreply.github.com>
2021-05-15 11:48:07 -0500

    Agregar archivos de proyecto.

---
+++ AdoptaPatitaMVC/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace AdoptaPatitaMVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
