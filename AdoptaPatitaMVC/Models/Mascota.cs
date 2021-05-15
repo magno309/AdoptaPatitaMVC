31712f07906661cb3d11ecc751e50d8086138bff
magno309 <48113838+magno309@users.noreply.github.com>
2021-05-15 11:59:21 -0500

    Creación de modelos

---
+++ AdoptaPatitaMVC/Models/Mascota.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AdoptaPatitaMVC.Models
+{
+    public class Mascota
+    {
+        public int Id_Mascota { get; set; }
+
+    }
+}
