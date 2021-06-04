47e8d4c5dc251a99a735e26fd87e541884724a42
magno309 <48113838+magno309@users.noreply.github.com>
2021-06-04 10:26:04 -0500

    Añadido filtro en base a atributos de la mascota

---
+++ AdoptaPatitaMVC/Models/BusquedaMascotaViewModel.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System.Collections.Generic;
+
+namespace AdoptaPatitaMVC.Models
+{
+    public class BusquedaMascotaViewModel
+    {
+        public List<Mascota> Mascotas { get; set; }
+        public SelectList Tamanios { get; set; }
+        public SelectList Sexos { get; set; }
+        public SelectList Edades { get; set; }
+        public string TamanioMascota { get; set; }
+        public string SexoMascota { get; set; }
+        public string EdadMascota { get; set; }
+
+    }
+}
